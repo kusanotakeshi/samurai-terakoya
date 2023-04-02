c8b09df2d3124d3d4af17dac6f054f7f011bc21c
kusanotakeshi <129176546+kusanotakeshi@users.noreply.github.com>
2023-04-02 20:19:44 +0900

    kadai_004 アップロード

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,23 @@
+package kadai_004;
+
+public class Score_Chapter04{
+
+public static void main(String[]args) {
+//	テストを受けた人の点数を出力する
+	System.out.println("Aさんの点数は" + 10 + "点");
+	System.out.println("Aさんの点数は" + 20 + "点");
+	System.out.println("Aさんの点数は" + 30 + "点");
+	System.out.println("Aさんの点数は" + 40 + "点");
+	System.out.println("Aさんの点数は" + 50 + "点");
+	System.out.println("Aさんの点数は" + 60 + "点");
+	System.out.println("Aさんの点数は" + 70 + "点");
+	System.out.println("Aさんの点数は" + 80 + "点");
+	System.out.println("Aさんの点数は" + 90 + "点");
+	System.out.println("Aさんの点数は" + 100 + "点");
+	
+	//	テストの平均点を出力する
+	System.out.println("テストの平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 +90 + 100 ) / 10 + "点" );
+	
+}
+
+}
